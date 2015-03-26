a4b470a4084d87ad56c97b5f2acaecbd2b63066b
gz0220 <gz0220@gmail.com>
2015-03-26 18:48:22 +0200

    indetation

--- tip.js
+++ tip.js
@@ -1,7 +1,7 @@
 (function (window) {
 
-	var tooltips = [];
-	
+    var tooltips = [];
+
     var defaults = {
         border: {
             color: 'rgba(255, 153, 51, 0.8)',
@@ -17,37 +17,37 @@
         tooltip: {
             border: 'solid 5px orange',
             orientation: 'bottom',
-			'background-color': 'gray',
-			color: 'orange'
+            'background-color': 'gray',
+            color: 'orange'
         }
     };
 
-    var backupStyle = function(element){
-		
-		var border = window.getComputedStyle(element).border;
-		var boxShadow = window.getComputedStyle(element).boxShadow;
-		var transition = window.getComputedStyle(element).transition;
-		var cursor = window.getComputedStyle(element).cursor;
+    var backupStyle = function (element) {
 
-		element.tipBackup = { 
-			border: border, 
-			boxShadow: boxShadow, 
-			transition: transition, 
-			cursor: cursor
-		};
-    }
+        var border = window.getComputedStyle(element).border;
+        var boxShadow = window.getComputedStyle(element).boxShadow;
+        var transition = window.getComputedStyle(element).transition;
+        var cursor = window.getComputedStyle(element).cursor;
 
-    var restoreBackup = function($element){
-		
-		var element = $element.get(0);
+        element.tipBackup = {
+            border: border,
+            boxShadow: boxShadow,
+            transition: transition,
+            cursor: cursor
+        };
+    };
 
-		$element.css({ 
-			border: element.tipBackup.border,
-			'box-shadow': element.tipBackup.boxShadow, 
-			transition: element.tipBackup.transition, 
-			cursor: element.tipBackup.cursor
-		});
-    }
+    var restoreBackup = function($element) {
+
+        var element = $element.get(0);
+
+        $element.css({
+            border: element.tipBackup.border,
+            'box-shadow': element.tipBackup.boxShadow,
+            transition: element.tipBackup.transition,
+            cursor: element.tipBackup.cursor
+        });
+    };
 
     var getOption = function (options, property, subProperty) {
 
@@ -67,53 +67,53 @@
         });
     };
 
-    var generateTooltipByOrientation = function($element, orientation, guid, backgroundColor){
+    var generateTooltipByOrientation = function($element, orientation, guid, backgroundColor) {
 
-		var html = '<div id="' + guid + '" style="position: absolute;display: none;">';
-		var triangle = '<div style="width:0px; margin:0 auto;border-' + orientation + ': solid 5px ' + backgroundColor + ';border-left: solid 5px transparent;border-right: solid 5px transparent;"></div>'
-		var content = '<div style="border: solid 1px  ' + backgroundColor + ';background-color: ' + backgroundColor + '; border-radius: 4px;padding: 4px;">' + $element.attr('tip-content') + '</div>';
+        var html = '<div id="' + guid + '" style="position: absolute;display: none;">';
+        var triangle = '<div style="width:0px; margin:0 auto;border-' + orientation + ': solid 5px ' + backgroundColor + ';border-left: solid 5px transparent;border-right: solid 5px transparent;"></div>';
+        var content = '<div style="border: solid 1px  ' + backgroundColor + ';background-color: ' + backgroundColor + '; border-radius: 4px;padding: 4px;">' + $element.attr('tip-content') + '</div>';
 
-		if(orientation == 'bottom'){
-			html+= triangle + content;
-		} else if(orientation == 'top'){
-			html+= content + triangle;
-		}
-            
-            
+        if (orientation == 'bottom') {
+            html += triangle + content;
+        } else if (orientation == 'top') {
+            html += content + triangle;
+        }
+
+
         html += '</div>';
 
         return html;
 
-    }
+    };
 
     var createTooltip = function ($element, boxShadow, backgroundColor, color, orientation) {
 
         var guid = generateGuid();
-		var elementTipOrientation = $element.attr('tip-orientation');
-		orientation = elementTipOrientation || orientation;
+        var elementTipOrientation = $element.attr('tip-orientation');
+        orientation = elementTipOrientation || orientation;
 
         var html = generateTooltipByOrientation($element, orientation, guid, backgroundColor);
 
         $(document.body).append(html);
         var $tooltip = $("#" + guid);
 
-        switch(orientation){
-			case "bottom":
-				$tooltip.position({ my: 'center top', at: 'center bottom', of: $element });
-				break;
-			case "top":
-				$tooltip.position({ my: 'center bottom', at: 'center top', of: $element });
-				break;
-		}
-		
-		// this is done here since position() overwrites style
-		$tooltip.css('color', color);
+        switch (orientation) {
+            case "bottom":
+                $tooltip.position({ my: 'center top', at: 'center bottom', of: $element });
+                break;
+            case "top":
+                $tooltip.position({ my: 'center bottom', at: 'center top', of: $element });
+                break;
+        }
+
+        // this is done here since position() overwrites style
+        $tooltip.css('color', color);
 
         $element.attr('tip-guid', guid);
 
         $element.mouseover(function () {
             $("#" + guid).show();
-            $(this).attr('tip-mouseover','');
+            $(this).attr('tip-mouseover', '');
         });
 
         $element.mouseout(function () {
@@ -135,8 +135,8 @@
 
         var intervalId = setInterval(function () {
 
-			var splitted = boxShadow.split(' ');
-			var boxShadowEnd = splitted[0] + ' ' + splitted[1] + ' 5px ' + splitted[3];
+            var splitted = boxShadow.split(' ');
+            var boxShadowEnd = splitted[0] + ' ' + splitted[1] + ' 5px ' + splitted[3];
 
             if (hasHeartbeat || $element.attr('tip-mouseover') != undefined) {
                 $element.css('box-shadow', boxShadowEnd);
@@ -162,10 +162,10 @@
         var heartbeatColor = getOption(options, 'heartbeat', 'color');
         var heartbeatRadius = getOption(options, 'heartbeat', 'radius');
         var heartbeatDuration = getOption(options, 'heartbeat', 'duration');
-		var tooltipBackgroundColor = getOption(options, 'tooltip', 'background-color');
-		var tooltipColor = getOption(options, 'tooltip', 'color');
-		var tooltipOrientation = getOption(options, 'tooltip', 'orientation');
-		
+        var tooltipBackgroundColor = getOption(options, 'tooltip', 'background-color');
+        var tooltipColor = getOption(options, 'tooltip', 'color');
+        var tooltipOrientation = getOption(options, 'tooltip', 'orientation');
+
 
         var boxShadow = '0px 0px ' + heartbeatRadius + 'px ' + heartbeatColor;
 
@@ -180,10 +180,10 @@
             if (heartbeatActive) {
                 setHeartbeat($self, boxShadow, heartbeatDuration);
             }
-			
-			if($self.attr('tip-content')){
-				$self.css('cursor', 'help');
-            	createTooltip($self, boxShadow, tooltipBackgroundColor, tooltipColor, tooltipOrientation);
+
+            if ($self.attr('tip-content')) {
+                $self.css('cursor', 'help');
+                createTooltip($self, boxShadow, tooltipBackgroundColor, tooltipColor, tooltipOrientation);
             }
         });
 
@@ -204,9 +204,9 @@
 
             restoreBackup($self);
 
-            tooltips.forEach(function(d) { 
-            	$("#" + d).hide();
-             })
+            tooltips.forEach(function(d) {
+                $("#" + d).hide();
+            });
 
             $tipped.unbind('mouseover').unbind('mouseout');
         });
